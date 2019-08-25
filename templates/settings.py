d0bcaadc082dca24615a08a8cb7aa9ed08e56b28
aaluo001 <aaluo008@163.com>
2019-08-25 10:17:51 +0800

    Modified settings.py

--- templates/settings.py
+++ templates/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'commons',
     'lists',
     'accounts',
     'functional_tests',
